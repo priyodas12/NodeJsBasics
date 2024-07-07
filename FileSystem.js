92833fde44fa0696e4f9cd5d9e26d0ae3c6cf6db
priyonrato.das <priyodas12@gmail.com>
2024-07-07 13:35:41 +0530

    write file

--- FileSystem.js
+++ FileSystem.js
@@ -1,6 +1,33 @@
 import fs from 'fs';
+import readline from 'readline';
+
+let textData = '';
 
 //read file synchronously
-const readFs = fs.readFileSync('./files/input.txt', 'utf-8');
+const readSync = fs.readFileSync('./files/input.txt', 'utf-8');
 
-console.log(readFs);
+console.log(readSync);
+
+const rlTextInput = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+rlTextInput.question('Write something to persist::', (data) => {
+	textData =
+		'Updated on :' +
+		new Date() +
+		'\n' +
+		data +
+		'\n' +
+		readSync +
+		'\n' +
+		textData;
+	fs.writeFileSync('./files/writeData.txt', textData);
+	rlTextInput.close();
+});
+
+rlTextInput.on('close', () => {
+	console.log('closing interface!');
+	process.exit(0);
+});
