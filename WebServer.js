05473529dfc532f0d7cf57419a329da17908483c
priyonrato.das <priyodas12@gmail.com>
2024-07-07 15:10:01 +0530

    why circular-json used

--- WebServer.js
+++ WebServer.js
@@ -2,6 +2,9 @@ import http from 'http';
 import stringify from 'flatten';
 import CircularJSON from 'circular-json';
 
+// Circular References in req Object: Common circular references in req can be found in properties like req.socket or in parsed request bodies,
+// especially if they refer back to parts of the req object itself.
+
 const httpServer = http.createServer((req, res) => {
 	//console.log('Request Method:', req.method);
 	//console.log('Request URL:', req.url);
